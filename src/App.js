26b3a420d56f55f621a060fbe2d18a71941507bc
triplehop <jwillcock00@gmail.com>
2020-07-13 21:57:51 +1000

    Added Navbar styling *to be rechanged

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from "./components/about/About";
 import Blog from "./components/blog/Blog";
 import Projects from "./components/projects/Projects";
 import Skills from "./components/skills/Skills";
+import Navbar from "./components/shared/Navbar"
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div>
+              <Navbar />
               <Switch>
                 <div>
               <Route exact path ={"/"} component={About} />
